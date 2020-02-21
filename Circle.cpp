2a8c3e163aa80dae9dd934f7237702e33ecd3959
Andy <chickenweiwang@gmail.com>
2020-02-21 00:03:17 -0500

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 class Circle {
-  private:  
+    private:
     int x,y;      // center of circle
     double r;    // radius of circle
 
-  public:
-   
+    public:
     Circle(int =0,int=0, double =1.0);
     void print() const;
     double area() const;
@@ -17,84 +16,83 @@ class Circle {
     Circle & operator++();
     Circle operator++(int);
 
-  friend ostream & operator<<(ostream &out, const Circle &);
-  friend istream & operator>>(istream &in, Circle &);
+    friend ostream & operator<<(ostream &out, const Circle &);
+    friend istream & operator>>(istream &in, Circle &);
 };
 
 Circle::Circle(int x,int y, double r)
 {
-  // Initialize the object here
+    // Initialize the object here
+    this->x = x;
+    this->y = y;
+    this->r = r;
 }
 
 double Circle::area() const {
-  // Return the area of a circle  
+    // Return the area of a circle
+    return (double)((this->r)*(this->r)*(3.14));
 }
 
 double Circle::operator+(const Circle & c) const
 {
-   // + Return the area of two circles  
-
+    // + Return the area of two circles
+    return this->area() + c.area();
 }
 
 Circle & Circle::operator++()
 {
-  // Increment the radius by 1
-
-  return *this;
+    // Increment the radius by 1
+    ++this->r;
+    return *this;
 }
 
 Circle Circle::operator++(int)
 {
-  Circle temp = *this;
-  
-  //increment the radius by 1
-
-  return temp;
+    Circle temp = *this;
+    //increment the radius by 1
+    temp.r = r++;
+    return temp;
 }
 
 ostream & operator<<(ostream &out, const Circle &c)
 {
-
-  // print a circle in the out stream (the format is the same as print())
-  return out;
+    // print a circle in the out stream (the format is the same as print())
+    out << "Circle =(" << c.x << "," << c.y << "," << c.r << ")" << endl;
+    return out;
 }
 
 istream & operator>>(istream &in, Circle &c)
 {
- 
- // Read a circle information form the in stream
-
-  return in;
+    // Read a circle information form the in stream
+    cin >> c.x >> c.y >> c.r;
+    return in;
 }
 
 
 void Circle::print() const
 {
-  cout << "Circle =(" << x << "," << y << "," << r << ")" << endl;
+    cout << "Circle =(" << x << "," << y << "," << r << ")" << endl;
 }
-
-
 
 int main()
 {
-  // Circle a;
-  // Circle b(2,3,3.0);
+    Circle a;
+    Circle b(2,3,3.0);
 
-  // a.print();
-  // b.print();
-  
-  // cout << "The area a + b is: " << a+b << endl;
-  // ++a;
-  // a.print();
+    a.print();
+    b.print();
 
-  // a++;
-  // a.print();
+    cout << "The area a + b is: " << a+b << endl;
+    ++a;
+    a.print();
 
-  // cout << a;
+    a++;
+    a.print();
 
-  // cin >> a;
-  // cout <<a;
+    cout << a;
+
+    cin >> a;
+    cout <<a;
 
   return 0;
-
 }
